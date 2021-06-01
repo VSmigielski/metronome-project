24faf0aa35bec3ea2ad77c73063a9a59c821e8ec
Veronica <veronicasmigielski@gmail.com>
2021-06-01 15:17:43 +0200

    Updated to add audio for clicks, changed states to swap the bpm rate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Metronome from './Metronome';
+
+function App() {
+  return (
+    <div className="App">
+      <Metronome />
+    </div>
+  );
+}
+
+export default App;
